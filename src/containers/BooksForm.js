6a88f8dd580745d829a70532f34b62209ae69a5a
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-02-08 17:04:11 -0600

    docs: Adding state to BooksForm

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BooksForm = () => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const [state, setState] = useState(
+    {
+      title: '',
+      category: '',
+    },
+  );
+
+  console.log(state);
+
+  const handleClick = event => {
+    if (event.target.name === 'title') {
+      setState({
+        category: state.category,
+        title: event.target.value,
+      });
+    } else {
+      setState({
+        category: event.target.value,
+        title: state.title,
+      });
+    }
+  };
+
   return (
     <form>
       <label htmlFor="title">
         Title:
-        <input type="text" id="title" />
+        <input type="text" id="title" name="title" onChange={handleClick} value={state.title} />
       </label>
       <br />
 
       <label htmlFor="select">
         Categories:
-        <select name="category" id="select">
+        <select name="category" id="select" onChange={handleClick} value={state.category}>
           {categories.map(category => <option value={category} key={category}>{category}</option>)}
         </select>
       </label>

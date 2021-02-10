0bd92574cc52655627fe0a4061645b81ed22f31f
jamezjaz <jamezjaz@gmail.com>
2021-02-10 12:04:01 -0600

    docs: Adds filterReducer

---
+++ src/reducers/filter.js
@@ -0,0 +1,16 @@
+import { CHANGE_FILTER } from "../actions";
+
+const filter = null;
+
+const filterReducer = (state = filter, action) => {
+  switch(action.type) {
+    case CHANGE_FILTER:
+      return {
+        filter: action.filter
+      }
+    default:
+      return state
+  }
+}
+
+export default filterReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import bookReducer from './books';
+import filterReducer from './filter';
 
 const rootReducer = combineReducers({
   book: bookReducer,
+  filter: filterReducer,
 });
 
 export default rootReducer;

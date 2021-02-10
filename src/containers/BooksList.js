dbc7d795920eb0c65f809f0adee92825a78b2fb0
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-02-10 12:33:07 -0600

    docs: Creating handleFilterChang function

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
-import { removeBookAction } from '../actions';
+import { removeBookAction, changeFilterAction } from '../actions';
+import CategoryFilter from '../components/CategoryFilter';
 
 const BooksList = props => {
   const { books } = props;
@@ -12,8 +13,15 @@ const BooksList = props => {
     removeBook(book);
   };
 
+  const handleFilterChange = category => {
+    const { filter } = props;
+    filter(category);
+  };
+
   return (
     <div>
+      <CategoryFilter handleFilter={handleFilterChange} />
+
       <table style={{ width: '100%' }}>
         <thead>
           <tr>
@@ -36,12 +44,16 @@ BooksList.propTypes = {
     category: PropTypes.string,
   })).isRequired,
   removeBook: PropTypes.func.isRequired,
+  filter: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({ books: state.book });
 const mapDispatchToProps = dispatch => ({
   removeBook: book => {
     dispatch(removeBookAction(book));
+  },
+  filter: category => {
+    dispatch(changeFilterAction(category));
   },
 });
 

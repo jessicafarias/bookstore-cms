3da8a1dd9b6380ea25f7a81549a7be1cce908d78
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-02-12 12:28:53 -0600

    style: Adding an interactive svg

--- src/components/Book.js
+++ src/components/Book.js
@@ -19,7 +19,29 @@ const Book = props => {
             </div>
           </div>
           <div>
-            <p>65%</p>
+            <div>
+              <div className="flex-wrapper">
+                <div className="single-chart">
+                  <svg viewBox="0 0 36 36" className="circular-chart blue">
+                    <path
+                      className="circle-bg"
+                      d="M18 2.0845
+                      a 15.9155 15.9155 0 0 1 0 31.831
+                      a 15.9155 15.9155 0 0 1 0 -31.831"
+                    />
+                    <path
+                      className="circle"
+                      strokeDasharray="60, 100"
+                      d="M18 2.0845
+                      a 15.9155 15.9155 0 0 1 0 31.831
+                      a 15.9155 15.9155 0 0 1 0 -31.831"
+                    />
+                    <text x="18" y="20.35" className="percentage">60%</text>
+                  </svg>
+                </div>
+              </div>
+              <p>65%</p>
+            </div>
             <p>Completed</p>
           </div>
           <div>

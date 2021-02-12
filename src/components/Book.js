23fbde5b3b5b8b9ac9521356d08f8430dd0f3c61
jamezjaz <jamezjaz@gmail.com>
2021-02-12 13:11:34 -0600

    style: Adds circular progress bar

--- src/components/Book.js
+++ src/components/Book.js
@@ -18,31 +18,28 @@ const Book = props => {
               <p>Edit</p>
             </div>
           </div>
-          <div>
-            <div>
-              <div className="flex-wrapper">
-                <div className="single-chart">
-                  <svg viewBox="0 0 36 36" className="circular-chart blue">
-                    <path
-                      className="circle-bg"
-                      d="M18 2.0845
-                      a 15.9155 15.9155 0 0 1 0 31.831
-                      a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <path
-                      className="circle"
-                      strokeDasharray="60, 100"
-                      d="M18 2.0845
-                      a 15.9155 15.9155 0 0 1 0 31.831
-                      a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <text x="18" y="20.35" className="percentage">60%</text>
-                  </svg>
-                </div>
-              </div>
-              <p>65%</p>
+          <div className="percent-completed">
+            <div className="single-chart">
+              <svg viewBox="0 0 36 36" className="circular-chart blue">
+                <path
+                  className="circle-bg"
+                  d="M18 2.0845
+                  a 15.9155 15.9155 0 0 1 0 31.831
+                  a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+                <path
+                  className="circle"
+                  strokeDasharray="60, 100"
+                  d="M18 2.0845
+                  a 15.9155 15.9155 0 0 1 0 31.831
+                  a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+              </svg>
             </div>
-            <p>Completed</p>
+            <div>
+              <p>65%</p>
+              <p>Completed</p>
+            </div>
           </div>
           <div>
             <p>CURRENT CHAPTER</p>
